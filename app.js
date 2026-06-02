df214a97a65067c3cd93a2127726de38757bae53
Max <slimmemaxie@gmail.com>
2026-06-02 17:08:51 +0200

    Add dashboard screen and login skip if API keys exist

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+window.onload = function() { // if api keys already exist in local storage, skip login screen
+    const storedID = localStorage.getItem("clientID");
+    const storedSecret = localStorage.getItem("clientSecret");
+    
+    if (storedID && storedSecret) {
+        viewDashboard();
+    }
+}
+
 function SaveKeys() {
     const ID = document.getElementById("client-id").value;
     const Secret = document.getElementById("client-secret").value;
@@ -10,4 +19,12 @@ function SaveKeys() {
     localStorage.setItem("clientID", ID);
     localStorage.setItem("clientSecret", Secret);
     alert("Your keys have been saved :)");
+    viewDashboard();
+}
+
+function viewDashboard() {
+    const loginScreen = document.getElementById("setup");
+    const dashboardScreen = document.getElementById("dashboard");
+    loginScreen.classList.add("hidden");
+    dashboardScreen.classList.remove("hidden");
 }
