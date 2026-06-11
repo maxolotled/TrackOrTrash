215c408cf6391007410fdb44f5637fde3ad5f5fb
Max <slimmemaxie@gmail.com>
2026-06-11 21:08:13 +0200

    Change redirect uri

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let authToken = "";
-const redirectUri = "http://127.0.0.1:5500/index.html";
+const redirectUri = "https://maxolotled.github.io/TrackOrTrash";
 let currentTracksList = [];
 let currentTracksIndex = 0;
 let currentTracksType = "";
